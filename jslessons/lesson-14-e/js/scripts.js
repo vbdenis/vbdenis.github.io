89c615823cf5a29b2ac252500700d7208b3ae38f
vbdenis <vbdenis@yandex.ru>
2019-05-18 03:38:12 +0300

    edit lesson 14

--- jslessons/lesson-14-e/js/scripts.js
+++ jslessons/lesson-14-e/js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-	$('#modal').on('click', function() {
+	$('.main_nav li:eq(1), .main_btn, .main_btna').on('click', function() {
 		$('.overlay').fadeIn(500);
-		$('.modal').show(500);
+		$('.modal').slideDown(500);
+	});
+
+	$('.close').on('click', function() {
+		$('.modal').slideUp(500);
+		$('.overlay').fadeOut(500);
 	});
 });
